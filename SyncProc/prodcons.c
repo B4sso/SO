6513f741825e8ea72a51703108a6775d7590e797
B4sso <bicienzocorleone@hotmail.it>
2017-10-15 13:21:32 +0200

    Sorgente completata, aggiunto risultato test

--- SyncProc/prodcons.c
+++ SyncProc/prodcons.c
@@ -9,58 +9,67 @@
 
 #define BUFSIZE	10  //Dimensione del buffer
 
+
+/*
+    RISULTATI TEST:
+      Il processo PRODUCER viene schedulato in modo da iterare fino al raggiungimento
+      di BUFSIZE-1, seguito dal CONSUMER che itera fino al raggiungimento di 0.
+      
+      Non c'è overlapping\race condition.
+      La race condition è data dall scheduler.
+
+*/
+
+
+
+
 void makeStuff();
 void takeStuff();
 
-int main(){
-	int segment_id; //Riferimento assoluto al segmento allocato
-	int* shared_memory;//Puntatore ad int che ospiterà il riferimento
-	const int size = 1*sizeof(int);//Size del segmento di memoria
-	pid_t producer,consumer;
 
+int main(){
+	pid_t producer,consumer;
+	int segment_id = shmget (IPC_PRIVATE,512,S_IRUSR|S_IWUSR);
+    int *shared_memory = (int*) shmat(segment_id,NULL,0);
+    shared_memory[0] = 0;
 	if ((producer = fork()) < 0)
 		{
 			fprintf(stderr,"Generazione processo PRODUCER non riuscita");
 			exit(0);
 		}
 
-	if (producer == 0) //Se è il processo producer,esegui makeStuff
-			makeStuff();
-
+	if (producer == 0) 
+	{
+	   makeStuff((int*) shmat(segment_id,NULL,0));	
+	}
 	if ((consumer = fork()) < 0)
 		{
 			fprintf(stderr,"Generazione processo CONSUMER non riuscita");
 			exit(0);
 		}
-
-	if (consumer == 0) //Se è il processo producer,esegui makeStuff
-			takeStuff();
-
+	if (consumer == 0) takeStuff((int*) shmat(segment_id,NULL,0));
+	wait(NULL);
 	wait(NULL);
 }
 
 
-void makeStuff()
+void makeStuff(int* shared_memory)
 {
-	shared_memory = (int*) shmat(segment_id,NULL,0);
 	while(1)
 	{
-	if (counter == BUFSIZE-1) continue; //si blocca se il buffer è pieno
-		//FAI QUALCOSA
-	counter ++;
-	printf("\n CREA C: [%d]\n",counter);
+	    while(shared_memory[0] == BUFSIZE-1); //si blocca se il buffer è pieno
+		    //FAI QUALCOSA
+	    shared_memory[0] = shared_memory[0] + 1;
+	    printf("\n CREA C: [%d]\n",shared_memory[0]);
 	}
 }
 
-void takeStuff()
+void takeStuff(int *shared_memory)
 {	
 	while(1)
-{
-	shared_memory = (int*) shmat(segment_id,NULL,0);
-	while (counter == 0); //si blocca se il buffer è vuoto
-		//FAI QUALCOSA
-	counter --;
-	printf("\nPRESO C: [%d]\n",counter);
-
-}
+    {
+	    while (shared_memory[0] <= 0); //si blocca se il buffer è vuoto
+	    shared_memory[0] = shared_memory[0] - 1;
+	    printf("\nPRESO C: [%d]\n",shared_memory[0]);
+    }
 }
