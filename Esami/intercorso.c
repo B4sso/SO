cfdacd998ddb1ae977c22acd73db22650d508ef1
B4sso <bicienzocorleone@hotmail.it>
2017-11-20 21:07:32 +0100

    Finalizzato esercizio

--- Esami/intercorso.c
+++ Esami/intercorso.c
@@ -4,19 +4,37 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-pid_t newp(void);
-int main()
+pid_t newp(void); /*	Per leggibilità, implementiamo la fork() in una funzione wrapper
+				   *	che ci consentirà di gestirne gli errori in modo compatto.
+				   */
+int main()	
 {
 	pid_t A,B,C;
-	if ((A = newp()) == 0) printf("sciaobel\n");
+	A = newp();
+	//A stampa il PID PROCESSO PADRE
+	if (A == 0) printf("PID PROCESSO PADRE: %d\n",getppid()); //ERRORE: precedentemente era: getpid(). Svista.
 	else{
-			if ((B = newp()) == 0) printf("bellosciao\n");
+			//B stampa PADRE.PID + B.PID
+			B = newp();
+			if (B == 0) printf("PADRE.PID + B.PID = %d\n",getppid() + getpid());
 			else{
-					if ((C = newp()) == 0) printf("plswork\n");
-						else{
-								printf("vai col primo %d\n",wait(NULL));
-								printf("vai col secondo %d\n",wait(NULL));
-								printf("vai col terzo %d\n",wait(NULL));
+					//C stampa C.PID
+					if (C == 0) printf("C.PID = %d\n",getpid());
+						else{	
+								/*
+								* ERRORE: La prima versione implementava 3 x printf("messaggio %d",wait(NULL));
+								* La traccia non richiedeva una stampa "FIFO": il padre avrebbe dovuto
+								* attendere la terminazione di tutti e tre i processi figli per poi eseguire
+								* l'ultima istruzione.									
+								* L'output a video rimane invariato, con l'implementazione corrente di Ubuntu (data 10/2017).
+								*/
+								wait(NULL);
+								wait(NULL);
+								wait(NULL);
+
+								printf("Figlio %d TERMINATO\n",A);
+								printf("Figlio %d TERMINATO\n",B);
+								printf("Figlio %d TERMINATO\n",C);
 								return 0;
 							}}}
 
@@ -25,7 +43,7 @@ int main()
 pid_t newp(void)
 {
 	pid_t temp = fork();
-	if (temp < 0) abort();
-	return temp;
+	if (temp < 0) abort(); //Se temp < 0, abort verrà sempre eseguita sul padre
+	return temp; //Se temp >= 0, ritorna.
 }
 
